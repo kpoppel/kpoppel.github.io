bcea102b535b9876f5a4aebcdd458a68a125aa9d
kpoppel <4174578+kpoppel@users.noreply.github.com>
2019-08-23 15:42:46 +0200

    Update keyvalue.js

--- keyvalue.js
+++ keyvalue.js
@@ -82,7 +82,6 @@
 	window.open('http://scratch.mit.edu/users/' + username, '_blank');
     };
     ext.ajaxget = function(ajaxurl) {
-/*
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
@@ -92,8 +91,6 @@
 		};
 		xhttp.open("GET", ajaxurl, true);
 		xhttp.send();		
-*/
-       return ajaxurl;
     };
     ext.paste = function(username) {
 	window.open('http://pastebin.com/u/' + username, '_blank');
