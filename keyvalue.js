d52f2835961c4fc64f0a60a0333a13fc29403bd0
kpoppel <4174578+kpoppel@users.noreply.github.com>
2019-08-25 12:27:05 +0200

    Update keyvalue.js
    Add new blocks for lists of cloud variables.

--- keyvalue.js
+++ keyvalue.js
@@ -20,12 +20,14 @@
 
     var descriptor = {
         blocks: [
-	    ['R', 'Create Cloud Variable %s', 'create_cloud_variable', 'myName'],
-	    ['R', 'Get Cloud Variable %s', 'get_cloud_variable', 'variableURL'],
-	    ['w', 'Set %s to %s', 'set_cloud_variable', 'variableURL', 'value'],
-	    ['R', 'New ☁ %s', 'create_cloud_variable', 'myName'],
-	    ['R', '☁ %s ', 'get_cloud_variable', 'variableURL'],
-		
+	    ['R', '☁ New %s', 'create_cloud_variable', 'myName'],
+	    ['R', '☁ Get %s ', 'get_cloud_variable', 'variableURL'],
+	    ['w', '☁ Set %s to %s', 'set_cloud_variable', 'variableURL', 'value'],
+		['R', '☁ Get list element %n in %s', get_cloud_variable_from_list, 1, 'variableURL'],
+		['w', '☁ Set list element %n in %s to %s', 1, set_cloud_varialbe_from_list, 'variableURL', 'value'],
+		['-']
+		[' ', 'Get list element %n in %s', get_element_from_space_delimited_list, 1, 'list'],
+		[' ', 'Get list length of %s', get_space_delimited_list_length, 'list']
         ],
 
 	menus: {
@@ -68,6 +70,38 @@
               }
         });
     };
+
+    ext.get_cloud_variable_from_list =  function(index, varname) {
+        // Make an AJAX call to the keyvalue.xyz API
+        $.ajax({
+			  type: "GET",
+              url: 'https://api.keyvalue.xyz/'+ext.get_element_from_space_delimited_list(index, varname),
+              success: function( variable_value ) {
+                  callback(variable_value);
+              }
+        });
+
+	}
+
+    ext.set_cloud_variable_in_list =  function(index, varname, value) {
+        // Make an AJAX call to the keyvalue.xyz API
+        $.ajax({
+			  type: "POST",
+              url: 'https://api.keyvalue.xyz/'+ext.get_element_from_space_delimited_list(index, varname)+'/'+value,
+			  data: "",
+              success: function( variable_url ) {
+                  callback(variable_url);
+              }
+        });
+	}
+
+    ext.get_element_from_space_delimited_list =  function(index, varname) {
+		return varname.split(' ')[index-1];
+	}
+
+    ext.get_space_delimited_list_length =  function(varname) {
+		return varname.split(' ').length;
+	}
 
     ext._getStatus = function() {
       return { status:2, msg:'Ready' };
